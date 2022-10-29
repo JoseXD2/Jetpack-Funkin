cedc1ede6aacf65411af1ac6083df179e8a77c36
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-10-29 16:54:15 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -20,6 +20,10 @@ class TitleState extends MusicBeatState
 
 	override public function create():Void
 	{
+		#if android
+		FlxG.android.preventDefaultKeys = [BACK];
+		#end
+		
 		Paths.clearStoredMemory();
 		Paths.clearUnusedMemory();
 
